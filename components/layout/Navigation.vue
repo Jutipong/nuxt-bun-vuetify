12c60e486eb8f3ce8fed10ce7f325360c93c719a
Jutipong.Dev <techcom.ec20@gmail.com>
2023-09-20 23:17:29 +0700

    update navigation

--- components/layout/Navigation.vue
+++ components/layout/Navigation.vue
@@ -1,27 +1,31 @@
 <template>
   <!-- rail -->
-  <v-navigation-drawer elevation="2" rail expand-on-hover>
+  <v-navigation-drawer elevation="2" expand-on-hover rail>
     <v-list density="compact">
       <v-list-item
         prepend-avatar="https://randomuser.me/api/portraits/women/44.jpg"
         title="ABC"
-        subtitle="abc@gmailcom"></v-list-item>
+        subtitle="abc@gmail.com"></v-list-item>
     </v-list>
 
     <v-divider />
 
-    <v-list v-model:opened="open" color="primary" density="compact" variant="plain">
-      <!-- <v-list-item prepend-icon="mdi-home" title="Home"></v-list-item> -->
+    <v-list
+      v-model:opened="open"
+      color="primary"
+      density="compact"
+      variant="plain"
+      :lines="false"
+      nav>
+      <v-list-item prepend-icon="mdi-home" title="Home" value="/" to="/"></v-list-item>
 
       <v-list-group fluid value="Users">
         <template v-slot:activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-test-tube" title="Users"></v-list-item>
+          <v-list-item v-bind="props" prepend-icon="mdi-test-tube" title="Menu 1"></v-list-item>
         </template>
         <!-- <v-list-subheader>REPORTS</v-list-subheader> -->
 
         <v-list-item
-          class="text-xl-subtitle-1"
-          density="compact"
           v-for="([title, icon, to], i) in cruds"
           :key="i"
           :value="to"
@@ -33,13 +37,11 @@
 
       <v-list-group fluid value="Users2">
         <template v-slot:activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-test-tube" title="Users2"></v-list-item>
+          <v-list-item v-bind="props" prepend-icon="mdi-test-tube" title="Menu 2"></v-list-item>
         </template>
         <!-- <v-list-subheader>REPORTS</v-list-subheader> -->
 
         <v-list-item
-          class="text-xl-subtitle-1"
-          density="compact"
           v-for="([title, icon, to], i) in cruds2"
           :key="i"
           :value="to"
@@ -54,19 +56,9 @@
 
 <script lang="ts" setup>
 const open = ref(['Users']);
-
-const admins = [
-  ['Management', 'mdi-account-multiple-outline'],
-  ['Settings', 'mdi-cog-outline'],
-];
 const cruds = [
-  ['Demo1', 'mdi-circle-small', '/demo01'],
-  ['Demo2', 'mdi-circle-small', '/demo02'],
-  // ['Update', 'mdi-update','/demo03'],
-  // ['Delete', 'mdi-delete','/demo04'],
-  // ['Read', 'mdi-file-outline'],
-  // ['Update', 'mdi-update'],
-  // ['Delete', 'mdi-delete'],
+  ['Demo1', 'mdi-chevron-right', '/demo01'],
+  ['Demo2', 'mdi-chevron-right', '/demo02'],
 ];
-const cruds2 = [['Home', 'mdi-circle-small', '/']];
+const cruds2 = [['Demo03', 'mdi-chevron-right', '/demo03']];
 </script>
---
+++ pages/demo03.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <v-row>
+    <h1>Demo 03</h1>
+  </v-row>
+</template>
